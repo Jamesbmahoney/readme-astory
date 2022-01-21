916d47c07084b99f3418d78dcce815fc7106fc9e
James Barry <jbm03003@gmail.com>
2022-01-21 12:21:23 -0500

    added required packages, page, and question list

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const generatePage = require('./utils/generatedMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () =>{
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter steps required to install your project!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How would one use this app?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter detail description of use!');
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Who were your collaborators (List if any, otherwise leave blank'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['Apache', 'Wordpress', 'npm packages', 'Rust', 'GNU'],            
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please choose a license!');
+                    return false; 
+                }
+            }
+        }, 
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
